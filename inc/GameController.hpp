a0361be3749cc1f1a5b7aa9dce215b798bcf5054
ptaq <pptaszni@gmail.com>
2016-02-29 12:58:51 +0100

    Initialized repo with structure from previous contest https://github.com/ptaq666/CodingGame-CodeVsZombies.git

---
+++ inc/GameController.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+class GameController
+{
+public:
+    void startGame();
+};
---
+++ src/GameController.cpp
@@ -0,0 +1,8 @@
+#include "GameController.hpp"
+
+#define DEBUG_PRINT 1
+
+void GameController::startGame()
+{
+    std::cout << "Game started ... " << std::endl;
+}
